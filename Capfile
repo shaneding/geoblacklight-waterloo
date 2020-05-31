5caecd04a336f8f9dfb9cf43b6bc4fc3e14c990c
Shane Ding <s44ding@uwaterloo.ca>
2020-05-31 00:45:58 -0400

    Newest version

--- Capfile
+++ Capfile
@@ -38,4 +38,4 @@ require "capistrano/passenger"
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.1'
+set :rbenv_ruby, '2.6.5'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server "165.227.46.30", user: "deploy", roles: %w{app db web}
 
 # role-based syntax
 # ==================
