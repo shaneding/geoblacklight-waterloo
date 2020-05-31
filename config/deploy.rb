268647dda321205fa9ced09f21dc50b3b201b412
Shane Ding <s44ding@uwaterloo.ca>
2020-05-31 13:51:41 -0400

    Fixed uglifier

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "app"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/shaneding/geoblacklight-waterloo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
